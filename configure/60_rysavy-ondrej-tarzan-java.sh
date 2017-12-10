a32a591db580d8e0f28424dd68376dd6959d5b8e
Marek Rychly <marek.rychly@gmail.com>
2017-12-11 00:48:13 +0100

    Fixed target-build package filename (including version) of rysavy-ondrej-tarzan.

--- configure/60_rysavy-ondrej-tarzan-java.sh
+++ configure/60_rysavy-ondrej-tarzan-java.sh
@@ -8,7 +8,7 @@ DIST_ROTARZAN_BASE=https://github.com/rysavy-ondrej/Tarzan
 echo "*** configuring ${DIST_ROTARZAN_BASE}" >&2
 
 SPARK_DEF_CONF="${SPARK_HOME}/conf/spark-defaults.conf"
-ROTARZAN_NDX_SPARK_SHELL="${ROTARZAN_HOME}/ndx-spark-shell/target/ndx-spark-shell.jar"
+ROTARZAN_NDX_SPARK_SHELL="${ROTARZAN_HOME}/ndx-spark-shell/target/ndx-spark-shell"
 
 cd "${ROTARZAN_HOME}"
 mvn package
@@ -16,6 +16,9 @@ cd -
 
 # clean Maven cache for downloaded artefacts
 rm -rf ~/.m2/repository
+
+# look for the artefact (its filename depends on the package version)
+ROTARZAN_NDX_SPARK_SHELL=$(ls -d "${ROTARZAN_NDX_SPARK_SHELL}"*.jar | tr '\n' ',' | sed 's/,$//')
 
 if grep -q 'spark.jars\s' "${SPARK_DEF_CONF}"; then
 	sed -i "s|^\\(spark.jars\\s.*\\)\$|\\1,${ROTARZAN_NDX_SPARK_SHELL}|" "${SPARK_DEF_CONF}"
